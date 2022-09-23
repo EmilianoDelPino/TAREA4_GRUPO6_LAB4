b23de7339aa354ac9399dc0abb3bf64172dc5f8d
EmilianoDelPino <emidelpino96@gmail.com>
2022-09-23 19:50:02 -0300

    Update Ventana

--- src/Ejercicio/Ventana.java
+++ src/Ejercicio/Ventana.java
@@ -7,6 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Ventana extends JFrame {
 	
@@ -22,9 +25,23 @@ public class Ventana extends JFrame {
 		
 		button = new JButton();
 		button.setText("Ejercicio1");
-		button.setBounds(200, 300, 100, 20);
-		
+		button.setBounds(184, 122, 134, 44);		
 		getContentPane().add(button);
+		
+		JButton btnEjercicio = new JButton();
+		btnEjercicio.setText("Ejercicio2");
+		btnEjercicio.setBounds(184, 187, 134, 44);
+		getContentPane().add(btnEjercicio);
+		
+		JButton btnEjercicio_1 = new JButton();
+		btnEjercicio_1.setText("Ejercicio3");
+		btnEjercicio_1.setBounds(184, 252, 134, 44);
+		getContentPane().add(btnEjercicio_1);
+		
+		JLabel lblGrupoNro = new JLabel("GRUPO NRO: 6");
+		lblGrupoNro.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblGrupoNro.setBounds(51, 41, 100, 33);
+		getContentPane().add(lblGrupoNro);
 	}
 	
 	public void Visibilidad(boolean estado)
